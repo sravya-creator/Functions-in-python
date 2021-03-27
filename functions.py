fcb326f1ba8d2ee5a81a74ebad65cb9530e4e176
sravya-creator <ksrv149@gmail.com>
2021-03-27 23:32:38 +0530

    first commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+a=5
+b=6
+def sum1(a,b):
+	sum=a+b
+	return sum
+def sum2():
+	sum=a+b
+	print(sum)
+def sum3():
+	sum=a+b
+	return sum
+def sum4(a,b):
+	sum=a+b
+	print(sum)
+print(sum1(a,b))
+sum2()
+print(sum3())
+sum4(a,b)
